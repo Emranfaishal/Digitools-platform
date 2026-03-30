dfd3119186cb0a50e56282fe7c780f2752d74a22
Emranfaishal <emranfaishalrahat@gmail.com>
2026-03-30 14:48:54 +0600

    card add to

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import BannerFive from './components/banner-five/BannerFive'
 import BannerFour from './components/banner-four/BannerFour'
 import BannerSix from './components/banner-six/BannerSix'
+import BannerTree from './components/banner-tree/BannerTree'
 import BannerTwo from './components/banner-two/BannerTwo'
 import Banner from './components/banner/Banner'
 import Footer from './components/footer/Footer'
@@ -14,7 +15,7 @@ function App() {
       <Nav></Nav>
       <Banner></Banner>
       <BannerTwo></BannerTwo>
-
+      <BannerTree></BannerTree>
       <BannerFour></BannerFour>
       <BannerFive></BannerFive>
       <BannerSix></BannerSix>
--- src/components/banner-tree/BannerTree.jsx
+++ src/components/banner-tree/BannerTree.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+
+
+
+
 const BannerTree = () => {
     return (
         <div>
-            
+            <h2>tome nake vai tree</h2>
         </div>
     );
 };
